008cc0f4960f72e2ded2076a79be35065a6495fc
sonalbhayani <118657484+sonalbhayani@users.noreply.github.com>
2025-11-27 00:57:21 +0530

    Implement Map checks and conversion methods
    Added various methods to check if a Map is empty and convert it to an array.

---
+++ Map.js
@@ -0,0 +1,49 @@
+let map=new Map();
+
+// checkthe map is empty or not using size property
+if(map.size===0) 
+console.log("empty");
+else
+console.log("not Empty");
+
+// check the map is empty or not using for of loop
+let isEmpty=true;
+for(let [k,v] of map){
+    isEmpty=false;
+}
+if(isEmpty) 
+console.log("empty");
+else
+console.log("not Empty");
+
+// check the map is empty or not using keys() method of map
+let result= map.keys().next().done;
+if(result) 
+console.log("empty");
+else
+console.log("not Empty");
+
+// / check the map is empty or not using entries() method of map
+// console.log(map.entries().next().done);
+let result1= map.entries().next().done;
+if(result1) 
+console.log("empty");
+else
+console.log("not Empty");
+
+map.set(1,'abc');
+map.set(2,'xyz');
+map.set(3,'pqr');
+// convert map to array using [...spreat operator]
+console.log(map);
+let arr=[...map];
+console.log(arr)
+// convert map to array usinf Array.from() method
+let arr1=Array.from(map);
+console.log(arr1);
+// convert map to array using forEach() mathod
+let arr2=[];
+map.forEach((key,value)=>{
+    arr2.push({value,key});
+});
+console.log(arr2)
